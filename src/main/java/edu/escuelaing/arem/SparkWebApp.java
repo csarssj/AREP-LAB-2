296919a736bb4a267703f8f35783226f9cf1b5e8
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-01-31 12:43:43 -0500

    heroku

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -20,6 +20,6 @@ public class SparkWebApp
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhot)
     }
 }
